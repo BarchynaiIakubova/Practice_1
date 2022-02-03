ca8b7223968644fcbc68d969cb87798a584fb8ae
Barchynai <barchynai.iakubova@gmail.com>
2022-02-03 12:02:36 +0600

    second commit

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,39 +1,5 @@
 package com.company;
 
 public class Student {
-    private String nameStudent;
-    private String surname;
-    private int age;
-    private String borndate;
 
-    public void setNameStudent(String name) {
-        this.nameStudent = name;
-    }
-    public String getNameStudent() {
-        return nameStudent;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setAge(int age) {
-        if (age > 1) {
-            this.age = age;
-        }
-        else System.out.println("The value you entered is wrong");
-    }
-    public int getAge() {
-        return age;
-    }
-
-    public void setBorndate(String borndate) {
-        this.borndate = borndate;
-    }
-    public String getBorndate() {
-        return borndate;
-    }
 }
