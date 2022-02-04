b7ba94768a60c005645ce55f3f15f5e6aa24eec2
Barchynai <barchynai.iakubova@gmail.com>
2022-02-04 16:59:19 +0600

    12 commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Laptop laptop = new Laptop();
-    laptop.setLaptopName("HP");
-    laptop.setMemory(16);
-    laptop.setColor("Silver");
-    laptop.setCpu("Cor i 7");
-        System.out.println(laptop.getLaptopName() + " " + laptop.getColor() + " " + laptop.getCpu() + laptop.getMemory());
-     laptop.shutDown();
-     laptop.turnOn();
-
+        Triangle triangle = new Triangle();
+        triangle.a = 5;
+        triangle.b = 4;
+        triangle.c = 8;
+        triangle.area();
     }
-
 }
